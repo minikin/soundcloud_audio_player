5ac9bd24cc3f029f6c7329f84642bf4d4540303c
minikin <djminikin@gmail.com>
2019-10-15 10:38:16 +0200

    Update MainScreen

--- lib/src/app/screens/main_screen.dart
+++ lib/src/app/screens/main_screen.dart
@@ -1,3 +1,5 @@
+import 'package:audio/src/app/views/clipper_view.dart';
+import 'package:audio/src/app/views/painter_view.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
@@ -5,8 +7,16 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Waveform',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routes: {
+        '/': (context) => ClipperView(),
+        '/painter': (context) => PainterView(),
+      },
     );
   }
 }
