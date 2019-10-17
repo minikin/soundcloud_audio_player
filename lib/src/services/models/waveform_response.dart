210349919d1d1b63bae3d925689f224ddfe38c8e
Sasha Prokhorenko <djminikin@gmail.com>
2019-10-17 16:08:46 +0200

    Add WaveformResponse

---
+++ lib/src/services/models/waveform_response.dart
@@ -0,0 +1,65 @@
+library waveform_response;
+
+import 'dart:convert';
+
+import 'package:audio/src/services/serializers/serializers.dart';
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value/built_value.dart';
+import 'package:built_value/serializer.dart';
+import 'package:flutter/services.dart';
+
+part 'waveform_response.g.dart';
+
+abstract class WaveformResponse
+    implements Built<WaveformResponse, WaveformResponseBuilder> {
+  static Serializer<WaveformResponse> get serializer =>
+      _$waveformResponseSerializer;
+
+  factory WaveformResponse([updates(WaveformResponseBuilder b)]) =
+      _$WaveformResponse;
+
+  WaveformResponse._();
+
+  int get bits;
+
+  int get channels;
+
+  BuiltList<int> get data;
+
+  int get length;
+
+  @BuiltValueField(wireName: 'sample_rate')
+  int get sampleRate;
+
+  @BuiltValueField(wireName: 'samples_per_pixel')
+  int get sampleSize;
+
+  int get version;
+
+  String toJson() {
+    return json
+        .encode(serializers.serializeWith(WaveformResponse.serializer, this));
+  }
+
+  static WaveformResponse fromJson(String jsonString) {
+    return serializers.deserializeWith(
+        WaveformResponse.serializer, json.decode(jsonString));
+  }
+
+  static Future<WaveformResponse> loadWaveformDataItem(String filename) async {
+    final data = await rootBundle.loadString('assets/waveforms/$filename');
+    return WaveformResponse.fromJson(data);
+  }
+
+  static Future<List<WaveformResponse>> loadWaveformDataList(
+      List<String> fileList) async {
+    List<WaveformResponse> waveformList = [];
+
+    for (final fileName in fileList) {
+      final item = await loadWaveformDataItem(fileName);
+      waveformList.add(item);
+    }
+
+    return waveformList;
+  }
+}
