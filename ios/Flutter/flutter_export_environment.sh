5428bfc698c51140f2f8613e349955f24382b3ce
minikin <djminikin@gmail.com>
2019-10-30 15:24:49 +0100

    Update iOS project

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Developer/Personal/flutter-audio"
-export "FLUTTER_TARGET=/Developer/Personal/flutter-audio/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/developer/Documents/Developer/Personal/flutter-audio"
+export "FLUTTER_TARGET=/Users/developer/Documents/Developer/Personal/flutter-audio/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/developer/flutter/bin/cache/artifacts/engine/ios"
