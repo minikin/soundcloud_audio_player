a0a36ecfa3f2d576208efc4fb7bd67895a94e7d9
minikin <djminikin@gmail.com>
2019-11-06 14:08:21 +0100

    Update fastlane for iOS

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,9 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/developer/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/developer/Documents/Developer/Personal/flutter-audio"
-export "FLUTTER_TARGET=/Users/developer/Documents/Developer/Personal/flutter-audio/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/developer/flutter/bin/cache/artifacts/engine/ios-release"
 export "FLUTTER_BUILD_NAME=0.0.2"
-export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_BUILD_NUMBER=137"
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,8 @@ import 'dart:async';
 import 'package:audio/src/app/app.dart';
 import 'package:audio/src/app/utils/app_bloc_delegate.dart';
 import 'package:bloc/bloc.dart';
-import 'package:flutter/material.dart';
-
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   BlocSupervisor.delegate = AppBlocDelegate();
