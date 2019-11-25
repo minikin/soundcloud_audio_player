aba6885d9600b4b427a948986918c561f97ce9de
minikin <djminikin@gmail.com>
2019-11-25 11:13:33 +0100

    Deintegrate firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,9 @@
-import 'dart:async';
-
 import 'package:audio/src/app/app.dart';
 import 'package:audio/src/app/bloc/bloc.dart';
 import 'package:bloc/bloc.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   BlocSupervisor.delegate = AppBlocDelegate();
-
-  Crashlytics.instance.enableInDevMode = true;
-  FlutterError.onError = Crashlytics.instance.recordFlutterError;
-
-  runZoned<Future<void>>(() async {
-    runApp(const App());
-  }, onError: Crashlytics.instance.recordError);
+  runApp(const App());
 }
