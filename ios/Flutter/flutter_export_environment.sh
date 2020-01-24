f0f78e56f70e322c41a1097a6e8075ef52715a16
Sasha Prokhorenko <djminikin@gmail.com>
2020-01-24 15:44:25 +0100

    Update pubs

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Developer/Personal/flutter-audio"
-export "FLUTTER_TARGET=/Developer/Personal/flutter-audio/lib/main.dart"
+export "FLUTTER_ROOT=/Users/Sascha/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/Sascha/Documents/Developer/Personal/Source/flutter_audio"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/developer/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_FRAMEWORK_DIR=/Users/Sascha/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
