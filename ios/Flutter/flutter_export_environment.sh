82cb49008bb1b8369f8ceb117b9c5d7e57876a32
Sasha Prokhorenko <djminikin@gmail.com>
2020-10-17 21:07:33 +0200

    Update flutter

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/Sascha/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/Sascha/Documents/Developer/Personal/Source/flutter_audio"
+export "FLUTTER_ROOT=/Users/sashaprokhorenko/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/sashaprokhorenko/Documents/GitHub/soundcloud_flutter"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/Users/Sascha/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/sashaprokhorenko/Documents/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=0.0.7"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
